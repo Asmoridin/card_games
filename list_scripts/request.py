ebc9167b0a2f6d426ec3e1a27df6ee8f75c8951b
Steve P <steven.pow@gmail.com>
2025-02-26 18:50:36 -0500

    A random assortment of additional cards

--- list_scripts/request.py
+++ list_scripts/request.py
@@ -25,7 +25,7 @@ def make_call(number):
         return 0
     return 1
 
-for x in range(48330, 900000):
+for x in range(48391, 900000):
     time.sleep(1)
     RESULT = make_call(x)
     if RESULT == -1:
