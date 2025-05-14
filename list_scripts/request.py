bbc926c84787621c6067c34b611cbaae6007b49b
Steve P <steven.pow@gmail.com>
2025-05-13 20:08:26 -0400

    More CoC Decks added

--- list_scripts/request.py
+++ list_scripts/request.py
@@ -27,7 +27,7 @@ def make_call(number):
         return 0
     return 1
 
-for x in range(65810, 900000):
+for x in range(66537, 900000):
     time.sleep(1)
     RESULT = make_call(x)
     if RESULT == -1:
