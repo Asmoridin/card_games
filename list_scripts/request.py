53d233b2348e3d380f4e11cb4eb7c7bb69dcb05d
Steve P <steven.pow@gmail.com>
2025-05-15 18:01:07 -0400

    LCG work, and adding more L5R cards to DB

--- list_scripts/request.py
+++ list_scripts/request.py
@@ -27,7 +27,7 @@ def make_call(number):
         return 0
     return 1
 
-for x in range(67032, 900000):
+for x in range(68715, 900000):
     time.sleep(1)
     RESULT = make_call(x)
     if RESULT == -1:
