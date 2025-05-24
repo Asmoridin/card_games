8f8842273ee5d170a47af900c874a6e5500d0424
Steve P <steven.pow@gmail.com>
2025-05-23 22:54:41 -0400

    Various improvements

--- list_scripts/request.py
+++ list_scripts/request.py
@@ -27,7 +27,7 @@ def make_call(number):
         return 0
     return 1
 
-for x in range(68715, 900000):
+for x in range(72929, 900000):
     time.sleep(1)
     RESULT = make_call(x)
     if RESULT == -1:
