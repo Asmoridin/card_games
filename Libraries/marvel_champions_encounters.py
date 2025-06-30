1b6699b112e5f9927fd3ae811e4ce20f99c6cdf6
Steve P <133926116+Asmoridin@users.noreply.github.com>
2025-06-30 08:23:06 -0400

    Update marvel_champions_encounters.py

--- Libraries/marvel_champions_encounters.py
+++ Libraries/marvel_champions_encounters.py
@@ -27,7 +27,8 @@ modular_encounters = ['Bomb Scare', 'Masters of Evil', 'Under Attack', 'Legions 
 ]
 mojo_encounters = ['Crime', 'Fantasy', 'Horror', 'Sci-Fi', 'Sitcom', 'Western']
 thunderbolt_encounters = ['Gravitational Pull', 'Hard Sound', 'Pale Little Spider',
-    'Power of the Atom', 'Supersonic', 'Batroc', 'Growing Strong', 'Extreme Risk']
+    'Power of the Atom', 'Supersonic', 'Batroc', 'Growing Strong', 'Extreme Risk', 'Techno',
+    'Whiteout', ]
 modular_encounters = modular_encounters + mojo_encounters + thunderbolt_encounters
 
 encounters = []
