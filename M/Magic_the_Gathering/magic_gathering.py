fe314b9a103994a18a5033b990741dcb6ac6f0c2
Steve P <steven.pow@gmail.com>
2025-10-30 22:26:19 -0400

    Updating outputs

--- M/Magic_the_Gathering/magic_gathering.py
+++ M/Magic_the_Gathering/magic_gathering.py
@@ -496,9 +496,9 @@ commander_cat_fh.close()
 card_corrections = get_corrections(card_corrections_fh.readlines())
 
 SET_CHECK = 0
-CHECK_SET = 'Lorwyn'
-CHECK_AMOUNT = 301
-SET_CHECK += 15 # Extra basic lands
+CHECK_SET = 'Morningtide'
+CHECK_AMOUNT = 150
+SET_CHECK += 0 # Extra basic lands
 
 TOTAL_OWN = 0
 TOTAL_MAX = 0
