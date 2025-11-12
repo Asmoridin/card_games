dccb111605adf82556be2cb87cd9a66c904902d1
Steve P <133926116+Asmoridin@users.noreply.github.com>
2025-11-12 17:49:07 -0500

    Inc update 2025 11 12 (#164)
    * Update Star Wars LCG Used Traits.txt
    
    * Update Star Wars LCG Used Traits.txt
    
    * Update Star Wars LCG Objectives.txt
    
    * Update Star Wars LCG Objectives.txt
    
    * Update mtg_sets.py
    
    * Update Rubinia Soulsinger.txt
    
    * Update Liliana, Death's Majesty.txt
    
    * Update Sachi, Daughter of Seshiro.txt
    
    * Update magic_gathering.py
    
    * Create Ashling, the Extinguisher.txt
    
    * Create Karn, Legacy Reforged.txt
    
    * Create Jace, Vryn's Prodigy.txt
    
    * Create Jadar, Ghoulcaller of Nephalia.txt
    
    * Create Kiora, the Crashing Wave.txt
    
    * Create Mikaeus, the Unhallowed.txt
    
    * Create Liliana, Untouched by Death.txt
    
    * Update Current MTG Working.txt
    
    * Update MTGCardData.txt
    
    * Update MTG Card Corrections.txt

--- M/Magic_the_Gathering/magic_gathering.py
+++ M/Magic_the_Gathering/magic_gathering.py
@@ -803,7 +803,7 @@ if __name__ == "__main__":
     double_print(f"{len(creature_types)} total creature types", out_file_h)
     USED_TYPES = ['Wall', 'Necron', 'Human', 'Cleric', 'Goblin', 'Squirrel', 'Soldier', 'Sliver',
         'Wizard', 'Spider', 'Barbarian', 'Beast', 'Zombie', 'Elf', 'Warrior', 'Spirit', 'Dragon',
-        'Elemental','Bird', 'Merfolk', ]
+        'Elemental','Bird', 'Merfolk', 'Shaman']
     for del_type in USED_TYPES:
         del creature_types[del_type]
     creature_types = sorted(creature_types.items(), key=lambda x:(-1 * x[1], x[0]))
