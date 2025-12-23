620e734c6941f05ca28d7e1497910b864786066d
Steve P <steven.pow@gmail.com>
2025-12-22 21:27:43 -0500

    Updated with new inventory

--- M/Magic_the_Gathering/magic_gathering.py
+++ M/Magic_the_Gathering/magic_gathering.py
@@ -496,9 +496,9 @@ commander_cat_fh.close()
 card_corrections = get_corrections(card_corrections_fh.readlines())
 
 SET_CHECK = 0
-CHECK_SET = 'Shards of Alara'
-CHECK_AMOUNT = 249
-SET_CHECK += 15 # Extra basic lands
+CHECK_SET = 'Conflux'
+CHECK_AMOUNT = 145
+SET_CHECK += 0 # Extra basic lands
 PRINT_SET = False
 
 TOTAL_OWN = 0
